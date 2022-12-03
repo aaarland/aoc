3edce70f156682067bafcd65369a7aa40bc3bba7
Adrian <adrian@aswaa.dev>
2022-12-02 18:37:28 -0600

    started day two

--- src/twentytwentytwo.rs
+++ src/twentytwentytwo.rs
@@ -1,8 +1,7 @@
-
-pub fn day_one(lines: Vec<String>){
+pub fn day_one(lines: Vec<String>) {
     let mut all_total_calories: Vec<i32> = vec![];
     let mut total_calories = 0;
-    for line in lines{
+    for line in lines {
         if line.is_empty() {
             all_total_calories.push(total_calories);
             total_calories = 0;
@@ -14,11 +13,30 @@ pub fn day_one(lines: Vec<String>){
     total_calories = 0;
     let mut index = all_total_calories.len() - 1;
 
-    while index >= all_total_calories.len() - 3{
-
+    while index >= all_total_calories.len() - 3 {
         total_calories += all_total_calories[index];
-        print!("{} ",index);
+        print!("{} ", index);
         index -= 1;
     }
     println!("The max of calories is {}", total_calories)
+}
+
+
+fn check_letter(letter: Option<&str>){
+    match letter {
+        Some("X")
+        _ => todo!()
+    }
+}
+pub fn day_two(lines: Vec<String>) {
+    let all_letters = lines.iter().map(|line| line.split(' '));
+    for mut letters in all_letters{
+        match letters.next(){
+            Some("A") => todo!(),
+            Some("B") => todo!(),
+            Some("C") => todo!(),
+            Some(&_) => todo!(),
+            None => panic!()
+        }
+    }
 }
