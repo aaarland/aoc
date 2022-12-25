9d3cba3f942a748be839530123e0e6a1f223be5f
Aarland <adrian.aarland@gmail.com>
2022-12-25 20:09:08 +0100

    Finished day 6

--- src/twentytwentytwo.rs
+++ src/twentytwentytwo.rs
@@ -4,8 +4,8 @@ mod day_one;
 mod day_three;
 mod day_two;
 mod day_five;
-//End day 2
+mod day_six;
 
-pub fn get_all_funcs() -> [fn(Vec<String>); 5]{
-    return [day_one::day_one, day_two::day_two, day_three::day_three, day_four::day_four, day_five::day_five]
+pub fn get_all_funcs() -> [fn(Vec<String>); 6]{
+    return [day_one::day_one, day_two::day_two, day_three::day_three, day_four::day_four, day_five::day_five, day_six::day_six]
 }
---
+++ src/twentytwentytwo/day_six.rs
@@ -0,0 +1,20 @@
+use std::collections::HashSet;
+
+pub fn day_six(lines: Vec<String>){
+    let mut index = 0;
+    for _ in lines[0].chars(){
+        let mut stack = HashSet::new();
+        for n in index..index+14{
+            if stack.contains(&lines[0].chars().nth(n).unwrap()){
+                break;
+            }
+            stack.insert(lines[0].chars().nth(n).unwrap());
+            // find only unique chars in stack
+            if n == index + 13 && stack.len() == 14 {
+                println!("{}", index + 14);
+                return;
+            }
+        }
+        index += 1;
+    }
+}
