c98b0b204bc0faa27edfe5852efa131c55aca702
Aarland <adrian.aarland@gmail.com>
2023-12-01 01:38:34 -0600

    feat: preparing day 2

--- src/twentytwentythree.rs
+++ src/twentytwentythree.rs
@@ -1,7 +1,8 @@
 use crate::solutions::AdventSolution;
 
 mod one;
+mod two;
 
 pub fn get_solutions() -> Vec<AdventSolution> {
-    vec![Box::new(one::DayOne)]
+    vec![Box::new(one::DayOne), Box::new(two::DayTwo)]
 }
---
+++ src/twentytwentythree/two.rs
@@ -0,0 +1,8 @@
+use crate::solutions::Solution;
+pub struct DayTwo;
+
+impl Solution for DayTwo {
+    fn solve(&self, lines: Vec<String>) -> () {
+        todo!()
+    }
+}
