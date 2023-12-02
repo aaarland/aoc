c641719ec23c9269bb7525cee1d946fd9f4cb8f4
aaarland <adrian.aarland@gmail.com>
2023-12-02 15:05:42 -0600

    feat: added all days for 2023

---
+++ src/twentytwentythree/four.rs
@@ -0,0 +1,31 @@
+
+struct DayFour {
+
+}
+
+impl Solve for DayThree {
+
+}
+
+fn part_one() {
+
+}
+
+fn part_two() {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+
+    }
+
+    #[test]
+    fn test_part_two() {
+
+    }
+}
---
+++ src/twentytwentythree/twenty.rs
@@ -0,0 +1,31 @@
+
+struct DayTwenty {
+
+}
+
+impl Solve for DayThree {
+
+}
+
+fn part_one() {
+
+}
+
+fn part_two() {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+
+    }
+
+    #[test]
+    fn test_part_two() {
+
+    }
+}
