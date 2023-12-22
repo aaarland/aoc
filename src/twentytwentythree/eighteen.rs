6b6592dad4085d8721abf2a1c8772d69fb817132
Aarland <adrian.aarland@gmail.com>
2023-12-22 13:31:21 -0500

    feat: started day 21 part 1

--- src/twentytwentythree/eighteen.rs
+++ src/twentytwentythree/eighteen.rs
@@ -1,6 +1,8 @@
-use std::fmt::Display;
+use std::{collections::VecDeque, fmt::Display};
 
-use crate::solutions::Solution;
+use image::GenericImage;
+
+use crate::{solutions::Solution, sprites};
 pub struct DayEighteen;
 
 impl Solution for DayEighteen {
@@ -72,16 +74,14 @@ impl Display for DigPlan {
 impl DigPlan {
     fn new() -> Self {
         DigPlan {
-            map: vec![vec![Terrain::Trench]],
-            width: 0,
-            height: 0,
+            map: vec![vec![Terrain::Ground; 600]; 600],
+            width: 600,
+            height: 600,
         }
     }
     fn dig(&mut self, pos: (usize, usize)) {
         self.width = self.width.max(pos.0 + 1);
         self.height = self.height.max(pos.1 + 1);
-        println!("Digging {:?}", pos);
-        println!("Width: {}, Height: {}", self.width, self.height);
         if self.map.get(pos.1).is_none() {
             self.map.push(vec![Terrain::Ground; self.width]);
         }
@@ -91,19 +91,91 @@ impl DigPlan {
             self.map.get_mut(pos.1).unwrap()[pos.0] = Terrain::Trench;
         }
     }
+
+    fn fill(&mut self) -> Self {
+        let mut new_map = self.map.clone();
+        let start = self.find_start();
+        let mut queue: VecDeque<(usize, usize)> = VecDeque::new();
+        queue.push_back(start);
+        while let Some(pos) = queue.pop_front() {
+            if pos.0 >= self.width || pos.1 >= self.height {
+                continue;
+            }
+            if new_map.get(pos.1).unwrap().get(pos.0).unwrap() == &Terrain::Trench {
+                continue;
+            }
+            if self.map.get(pos.1).unwrap().get(pos.0).unwrap() == &Terrain::Trench {
+                continue;
+            }
+            if self.map.get(pos.1).unwrap().get(pos.0).unwrap() == &Terrain::Ground {
+                new_map.get_mut(pos.1).unwrap()[pos.0] = Terrain::Trench;
+                queue.push_back((pos.0 + 1, pos.1));
+                if pos.0 != 0 {
+                    queue.push_back((pos.0 - 1, pos.1));
+                }
+                queue.push_back((pos.0, pos.1 + 1));
+                if pos.1 != 0 {
+                    queue.push_back((pos.0, pos.1 - 1));
+                }
+            }
+        }
+        DigPlan {
+            map: new_map,
+            width: self.width,
+            height: self.height,
+        }
+    }
+
+    fn find_start(&self) -> (usize, usize) {
+        for (y, row) in self.map.iter().enumerate() {
+            for (x, col) in row.iter().enumerate() {
+                if col == &Terrain::Trench {
+                    return (x + 1, y + 1);
+                }
+            }
+        }
+        panic!("No start found");
+    }
 }
 
+fn generate_image(dig_plan: &DigPlan) {
+    let sprite_size = 16;
+    let sprites = sprites::load_sprite("sprites/ground.png", sprite_size, 2, 1).unwrap();
+    let height = dig_plan.height;
+    let width = dig_plan.width;
+    let mut image =
+        image::DynamicImage::new_rgba8(width as u32 * sprite_size, height as u32 * sprite_size);
+    let ground = &sprites[0];
+    let trench = &sprites[1];
+
+
+    println!("{} {}", width, height);
+    for (y, row) in dig_plan.map.iter().enumerate() {
+        for (x, col) in row.iter().enumerate() {
+            let sprite = match col {
+                Terrain::Trench => trench,
+                Terrain::Ground => ground,
+            };
+            image.copy_from(sprite, x as u32 * sprite_size, y as u32 * sprite_size).unwrap();
+        }
+    }
+    image.save("day18.png").unwrap();
+}
 fn part_one(lines: Vec<String>) -> usize {
     let steps = lines.iter().map(|line| Step::from(line));
     let mut dig_plan = DigPlan::new();
-    let mut pos = (0, 0);
+    let mut pos = (250, 250);
     steps.for_each(|step| match step.direction {
         Direction::Up => {
             for _ in 0..step.count {
-                if pos.1 == 0 {
-                    panic!("Invalid position");
+                match pos.1 {
+                    0 => {
+                        dig_plan
+                            .map
+                            .insert(0, vec![Terrain::Ground; dig_plan.width]);
+                    }
+                    _ => pos.1 -= 1,
                 }
-                pos.1 -= 1;
                 dig_plan.dig(pos);
             }
         }
@@ -115,10 +187,12 @@ fn part_one(lines: Vec<String>) -> usize {
         }
         Direction::Left => {
             for _ in 0..step.count {
-                if pos.0 == 0 {
-                    panic!("Invalid position");
+                match pos.0 {
+                    0 => dig_plan.map.iter_mut().for_each(|row| {
+                        row.insert(0, Terrain::Ground);
+                    }),
+                    _ => pos.0 -= 1,
                 }
-                pos.0 -= 1;
                 dig_plan.dig(pos);
             }
         }
@@ -129,19 +203,15 @@ fn part_one(lines: Vec<String>) -> usize {
             }
         }
     });
-    dig_plan
-        .map
-        .iter_mut()
-        .for_each(|row| {
-            row.iter_mut()
-                .skip_while(|t| **t == Terrain::Ground)
-                .skip_while(|t| **t == Terrain::Trench)
-                .take_while(|t| **t == Terrain::Ground)
-                .for_each(|t| *t = Terrain::Trench);
-        });
 
     println!("{}", dig_plan);
-    dig_plan
+    // generate_image(&dig_plan);
+
+
+    let new_map = dig_plan.fill();
+
+    println!("{}", new_map);
+    new_map
         .map
         .iter()
         .map(|row| row.iter().filter(|t| **t == Terrain::Trench).count())
--- src/twentytwentythree/twentyone.rs
+++ src/twentytwentythree/twentyone.rs
@@ -1,31 +1,136 @@
+use std::{collections::{VecDeque, HashSet}, fmt::Display};
 
 use crate::solutions::Solution;
 pub struct DayTwentyOne;
 
 impl Solution for DayTwentyOne {
     fn solve(&self, lines: Vec<String>) -> () {
+        let part_one = part_one(lines.clone(), 64);
+        println!("Part one: {}", part_one);
     }
 }
 
-fn part_one() {
-
+#[derive(Clone, PartialEq)]
+enum Plots {
+    Garden,
+    Rock,
 }
 
-fn part_two() {
-
+struct Elf {
+    grid: Vec<Vec<Plots>>,
+    steps: HashSet<(usize, usize)>,
+    current_step: usize,
 }
+
+impl Display for Elf {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut s = String::new();
+
+        for y in 0..self.grid.len() {
+            for x in 0..self.grid[0].len() {
+                s.push(match self.grid[y][x] {
+                    Plots::Garden => {
+                        if self.steps.contains(&(x, y)) {
+                            'O'
+                        } else {
+                            '.'
+                        }
+                    }
+                    Plots::Rock => '#',
+                });
+            }
+            s.push('\n');
+        }
+
+        write!(f, "{}", s)
+    }
+}
+
+impl Elf {
+    fn new(grid: Vec<Vec<Plots>>) -> Self {
+        Elf {
+            grid,
+            steps: HashSet::new(),
+            current_step: 0,
+        }
+    }
+
+    fn walk(&mut self, x: usize, y: usize, total_steps: usize) {
+        self.steps.insert((x, y));
+        while self.current_step < total_steps {
+            let mut new_steps = HashSet::new();
+            for (x, y) in self.steps.clone().into_iter() {
+                new_steps.extend(self.add_steps(x, y));
+            }
+            self.steps = new_steps;
+            self.current_step += 1;
+        }
+    }
+
+    fn add_steps(&self, x: usize, y: usize) -> HashSet<(usize, usize)> {
+        let mut new_steps = HashSet::new();
+        if self.grid[y][x] == Plots::Rock {
+            return new_steps;
+        }
+        if x > 0 {
+            new_steps.insert((x - 1, y));
+        }
+        if y > 0 {
+            new_steps.insert((x, y - 1));
+        }
+        if x < self.grid[0].len() - 1 {
+            new_steps.insert((x + 1, y));
+        }
+        if y < self.grid.len() - 1 {
+            new_steps.insert((x, y + 1));
+        }
+        new_steps
+    }
+}
+
+fn part_one(lines: Vec<String>, total_steps: usize) -> usize {
+    let mut start = None;
+    let grid = lines
+        .iter()
+        .enumerate()
+        .map(|(y, l)| {
+            l.chars()
+                .enumerate()
+                .map(|(x, c)| match c {
+                    '.' => Plots::Garden,
+                    '#' => Plots::Rock,
+                    'S' => {
+                        start = Some((x, y));
+                        Plots::Garden
+                    }
+                    _ => panic!("Invalid input"),
+                })
+                .collect::<Vec<Plots>>()
+        })
+        .collect::<Vec<Vec<Plots>>>();
+    let mut elf = Elf::new(grid);
+    let (x, y) = start.unwrap();
+    elf.walk(x, y, total_steps);
+    println!("{}", elf);
+    println!("{:?}", elf.steps);
+    elf.steps.len()
+}
+
+fn part_two() {}
 
 #[cfg(test)]
 mod tests {
+    use crate::utils;
+
     use super::*;
 
     #[test]
     fn test_part_one() {
-
+        let lines = utils::read_file(&"2023/example21".into());
+        let part_one = part_one(lines.clone(), 6);
+        assert_eq!(part_one, 16);
     }
 
     #[test]
-    fn test_part_two() {
-
-    }
+    fn test_part_two() {}
 }
