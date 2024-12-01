a0a89ff489ccf5523bc69e7c432426f29c8d283d
Adrian Aarland <adrian.aarland@gmail.com>
2024-12-01 12:30:48 -0600

    feat: added 2024 entries

---
+++ src/2024/twenty.rs
@@ -0,0 +1,31 @@
+
+use crate::solutions::Solution;
+pub struct DayTwenty;
+
+impl Solution for DayTwenty {
+    fn solve(&self, lines: Vec<String>) -> () {
+    }
+}
+
+fn part_one() {
+
+}
+
+fn part_two() {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+
+    }
+
+    #[test]
+    fn test_part_two() {
+
+    }
+}
