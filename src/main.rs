df0099f2a9dc882d51dcfae22e30634239566346
Adrian Aarland <adrian.aarland@gmail.com>
2024-12-06 00:17:53 -0600

    feat: added sqlite to avoid storing files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use core::panic;
 use std::env;
+
+use sqlx::{Connection, SqliteConnection};
 
 use crate::solutions::AdventCalendarYear;
 
@@ -12,9 +15,38 @@ mod twentytwentythree;
 mod twentytwentytwo;
 mod utils;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    let mut connection = SqliteConnection::connect("sqlite:advent_data.db")
+        .await
+        .expect("failed to connect to db lol");
+
     let config = menu::Config::new(env::args()).unwrap();
-    let lines = utils::read_file(&config.file);
+    let day = config.day as i32;
+    let year = config.year as i32;
+    let new_lines = sqlx::query!(
+        "SELECT example, full FROM aoc where day = ? and year = ?",
+        day,
+        year
+    )
+    .fetch_all(&mut connection)
+    .await
+    .expect("Failed to query lol");
+    let lines = new_lines
+        .iter()
+        .map(|thing| match config.file {
+            _ if config.file.contains("example") => thing.example.clone().unwrap(),
+            _ if config.file.contains("day") => thing.full.clone().unwrap(),
+            _ => panic!("couldn't find example or day in file name"),
+        })
+        .flat_map(|result| {
+            result
+                .split('\n')
+                .map(|string| string.to_string())
+                .collect::<Vec<_>>()
+        })
+        .collect();
+    //let lines = utils::read_file(&config.file);
     println!("Day {} : In file {}", config.day, config.file);
 
     let advent_funcs_2021 = [
--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,59 @@
+use core::panic;
 use std::{
     fs::File,
     io::{BufRead, BufReader},
 };
+
+use regex::Regex;
 
 pub fn read_file(location: &String) -> Vec<String> {
     let file = File::open(location).expect("Could not open file");
     let buf = BufReader::new(file);
     buf.lines()
         .map(|l| l.expect("Could not parse line"))
+        .collect()
+}
+
+pub fn extract_day_year(file: &str) -> (i32, i32) {
+    let re = Regex::new(r"^(\d{4})/(?:day|example)(\d+)$").unwrap();
+
+    let Some(captures) = re.captures(file) else {
+        panic!("Could not parse string {file}");
+    };
+    let year = captures.get(1).unwrap().as_str();
+    let day = captures.get(2).unwrap().as_str();
+    (
+        day.parse().expect("Failed to parse day {file}"),
+        year.parse().expect("Failed to parse year {year}"),
+    )
+}
+
+pub async fn read_db(
+    connection: &mut sqlx::SqliteConnection,
+    day: i32,
+    year: i32,
+    file: String,
+) -> Vec<String> {
+    let new_lines = sqlx::query!(
+        "SELECT example, full FROM aoc where day = ? and year = ?",
+        day,
+        year
+    )
+    .fetch_all(connection)
+    .await
+    .expect("Failed to query lol");
+    new_lines
+        .iter()
+        .map(|thing| match file {
+            _ if file.contains("example") => thing.example.clone().unwrap(),
+            _ if file.contains("day") => thing.full.clone().unwrap(),
+            _ => panic!("couldn't find example or day in file name"),
+        })
+        .flat_map(|result| {
+            result
+                .split('\n')
+                .map(|string| string.to_string())
+                .collect::<Vec<_>>()
+        })
         .collect()
 }
