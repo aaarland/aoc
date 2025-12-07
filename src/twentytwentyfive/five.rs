a5de4eacc8c2c9d5b655dcb3f082760bb653c7d2
Adrian Aarland <adrian.aarland@gmail.com>
2025-12-06 19:40:55 -0600

    wip: updated 5-25

--- src/twentytwentyfive/five.rs
+++ src/twentytwentyfive/five.rs
@@ -1,5 +1,5 @@
-use std::collections::HashSet;
 use rayon::prelude::*;
+use std::collections::HashSet;
 
 use crate::define_solution;
 
@@ -48,21 +48,43 @@ fn part_one(lines: Vec<String>) -> String {
 
 fn part_two(lines: Vec<String>) -> String {
     let lines_iter = lines.into_iter();
-    let ranges: Vec<Range> = lines_iter
+    let mut ranges: Vec<Option<Range>> = lines_iter
         .take_while(|line| !line.is_empty())
-        .map(|next| Range::from(next.as_str())).collect();
+        .map(|next| Some(Range::from(next.as_str())))
+        .collect();
+    let mut updated = false;
+    let range_len = ranges.len();
+    while !updated {
+        updated = false;
+        for i in (0..range_len) {
+            if let Some(range) = maybe_range {
+                for (j, maybe_check_range) in ranges.iter().enumerate() {
+                    if let Some(check_range) = maybe_check_range{
+                        if is_num_in_range(&range, check_range.lower) || is_num_in_range(&range, check_range.upper){
+                            range.lower = check_range.lower.min(range.lower);
+                            range.upper = check_range.upper.max(range.upper);
+                            updated = true;
+                            ranges[j] = None;
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+    "".to_string()
     // we need to combine ranges.
-    let set_of_ranges = ranges.into_par_iter().fold(|| HashSet::new(), |mut acc: HashSet<u64>, range| {
-        for i in range.lower..=range.upper {
-            acc.insert(i);
-        }
-        acc
-    }).collect_vec_list();
-    println!("Collected all ranges");
-        set_of_ranges.into_iter().fold(HashSet::new(), |mut acc: HashSet<u64>, next| {
-        for set in next {
-            acc.extend(set);
-        }
-        acc
-    }).len().to_string()
+    // let set_of_ranges = ranges.into_par_iter().fold(|| HashSet::new(), |mut acc: HashSet<u64>, range| {
+    //     for i in range.lower..=range.upper {
+    //         acc.insert(i);
+    //     }
+    //     acc
+    // }).collect_vec_list();
+    // println!("Collected all ranges");
+    //     set_of_ranges.into_iter().fold(HashSet::new(), |mut acc: HashSet<u64>, next| {
+    //     for set in next {
+    //         acc.extend(set);
+    //     }
+    //     acc
+    // }).len().to_string()
 }
